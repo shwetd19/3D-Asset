318b00a73debde5b17ca3efcf783644ce1a0f245
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-07-12 19:11:48 +0530

    Main file fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {  Hero, StarsCanvas } from "./components";
+import { Hero, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           {/* <Navbar /> */}
           <Hero />
         </div>
-        {/* <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks /> */}
-        <div className='relative z-0'>
-          {/* <Contact /> */}
-          {/* <StarsCanvas /> */}
-        </div>
+        <div className="relative z-0"></div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
